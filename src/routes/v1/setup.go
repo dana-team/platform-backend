fd3829df375147219382f4faa5bde41f4591c4d9
Ran Faingersh <fran2810@gmail.com>
2024-07-11 16:28:00 +0300

    refactor: removed slahes
    Signed-off-by: Ran Faingersh <fran2810@gmail.com>

--- src/routes/v1/setup.go
+++ src/routes/v1/setup.go
@@ -19,23 +19,23 @@ func SetupRoutes(engine *gin.Engine, tokenProvider auth.TokenProvider) {
 
 	authGroup := v1.Group("/login")
 	{
-		authGroup.POST("/", Login(tokenProvider))
+		authGroup.POST("", Login(tokenProvider))
 	}
 
 	namespacesGroup := v1.Group("/namespaces")
 	namespacesGroup.Use(middleware.TokenAuthMiddleware(tokenProvider))
 	{
-		namespacesGroup.GET("/", GetNamespaces())
+		namespacesGroup.GET("", GetNamespaces())
 		namespacesGroup.GET("/:namespaceName", GetNamespace())
-		namespacesGroup.POST("/", CreateNamespace())
+		namespacesGroup.POST("", CreateNamespace())
 		namespacesGroup.DELETE("/:namespaceName", DeleteNamespace())
 	}
 
 	secretsGroup := namespacesGroup.Group("/:namespaceName/secrets")
 	secretsGroup.Use(middleware.TokenAuthMiddleware(tokenProvider))
 	{
-		secretsGroup.POST("/", CreateSecret())
-		secretsGroup.GET("/", GetSecrets())
+		secretsGroup.POST("", CreateSecret())
+		secretsGroup.GET("", GetSecrets())
 		secretsGroup.GET("/:secretName", GetSecret())
 		secretsGroup.PUT("/:secretName", UpdateSecret())
 		secretsGroup.DELETE("/:secretName", DeleteSecret())
@@ -44,8 +44,8 @@ func SetupRoutes(engine *gin.Engine, tokenProvider auth.TokenProvider) {
 	cappGroup := namespacesGroup.Group("/:namespaceName/capps")
 	cappGroup.Use(middleware.TokenAuthMiddleware(tokenProvider))
 	{
-		cappGroup.POST("/", CreateCapp())
-		cappGroup.GET("/", GetCapps())
+		cappGroup.POST("", CreateCapp())
+		cappGroup.GET("", GetCapps())
 		cappGroup.GET("/:cappName", GetCapp())
 		cappGroup.PUT("/:cappName", UpdateCapp())
 		cappGroup.DELETE("/:cappName", DeleteCapp())
@@ -54,15 +54,15 @@ func SetupRoutes(engine *gin.Engine, tokenProvider auth.TokenProvider) {
 	cappRevisionGroup := namespacesGroup.Group("/:namespaceName/capprevisions")
 	cappRevisionGroup.Use(middleware.TokenAuthMiddleware(tokenProvider))
 	{
-		cappRevisionGroup.GET("/", GetCappRevisions())
+		cappRevisionGroup.GET("", GetCappRevisions())
 		cappRevisionGroup.GET("/:cappRevisionName", GetCappRevision())
 	}
 
 	usersGroup := namespacesGroup.Group("/:namespaceName/users")
 	usersGroup.Use(middleware.TokenAuthMiddleware(tokenProvider))
 	{
-		usersGroup.POST("/", CreateUser())
-		usersGroup.GET("/", GetUsers())
+		usersGroup.POST("", CreateUser())
+		usersGroup.GET("", GetUsers())
 		usersGroup.GET("/:userName", GetUser())
 		usersGroup.PUT("/:userName", UpdateUser())
 		usersGroup.DELETE("/:userName", DeleteUser())
