391323d6eaad8e42e1a77bb380e1ae789cb1300c
Ran Faingersh <fran2810@gmail.com>
2024-07-23 13:22:04 +0300

    tests: added unit tests to the namespace controller
    Signed-off-by: Ran Faingersh <fran2810@gmail.com>

--- src/utils/labels.go
+++ src/utils/labels.go
@@ -1,13 +1,17 @@
 package utils
 
-import cappv1alpha1 "github.com/dana-team/container-app-operator/api/v1alpha1"
+import (
+	"fmt"
+	cappv1alpha1 "github.com/dana-team/container-app-operator/api/v1alpha1"
+)
 
-var domain = cappv1alpha1.GroupVersion.Group
-var managedLabel = domain + "/managed"
-var ManagedLabelSelector = managedLabel + "=true"
+var cappAPIGroup = cappv1alpha1.GroupVersion.Group
+var ManagedLabel = cappAPIGroup + "/managed"
+var ManagedLabelValue = "true"
+var ManagedLabelSelector = fmt.Sprintf("%s=%s", ManagedLabel, ManagedLabelValue)
 
 // AddManagedLabel adds the managed label to the given labels map.
 func AddManagedLabel(labels map[string]string) map[string]string {
-	labels[managedLabel] = "true"
+	labels[ManagedLabel] = "true"
 	return labels
 }
